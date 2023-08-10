7ed6c9893d5038929e2a2771dea5d7735f5e02fa
denene imbatia chancella <chancellaimbatiadenene@gmail.com>
2023-08-10 15:16:21 +0000

    d

--- diagramme/final.sql
+++ diagramme/final.sql
@@ -18,11 +18,11 @@ create table tuteur
 
 INSERT INTO tuteur (nom, prenom, date_de_naissance, ville, contact)
 VALUES ('AGBANOU','Vital','2003-07-12','Nantes',0022996547895),
-       (),
-       (),
-       (),
-       (),x
-       (),
+       ('BABOLO','Aristide','1960-05-09','Bouar',0023766253805),
+       ('SANOU','Prisca','2000-06-11','Kaya',00022679023456),
+       ('OUEDRAGO','Jean','1995-02-05','Bobo',0022678336021),
+       ('SAMDOGO','Ilifou','1987-01-10','Dori',0022660251245),
+       ('KEREGOULE','Martina','2005-06-12','ouaga',0022675201605),
        
 
 create table apprenant
